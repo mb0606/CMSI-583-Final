95d63d11c29a3768905a166dfe3e825a65581404
mb0606 <marcoberardini@gmail.com>
2021-12-07 12:44:04 -0800

    Create Cell for tape

---
+++ tape.js
@@ -0,0 +1,8 @@
+class Cell {
+
+    constructor(sym) {
+        this.symbole = sym; // 
+        this.prev = null; // reference to the prev node
+        this.next = null; // . reference to next node
+    }
+}
---
+++ turingMachine.js
@@ -0,0 +1,1 @@
+import Tape from "./tape"
