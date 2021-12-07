23dc1de13922e15e58b0d175c299d248728e4e63
mb0606 <marcoberardini@gmail.com>
2021-12-07 15:53:56 -0800

    create turing machine class and constructor

--- turingMachine.js
+++ turingMachine.js
@@ -1,1 +1,9 @@
 import Tape from "./tape"
+
+export default class TuringMachine {
+    constructor({ start, accepts, transitions }) {
+      this.startState = start;
+      this.acceptStates = accepts;
+      this.transitions = transitions;
+    }
+  }
