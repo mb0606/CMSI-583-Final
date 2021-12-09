403c8e389a97340fa270bc20d4fb04344234da1d
mb0606 <marcoberardini@gmail.com>
2021-12-09 14:38:40 -0800

    add turing machine methods

--- turingMachine.js
+++ turingMachine.js
@@ -1,9 +1,56 @@
-import Tape from "./tape"
+import Tape from "./tape";
 
 export default class TuringMachine {
-    constructor({ start, accepts, transitions }) {
-      this.startState = start;
-      this.acceptStates = accepts;
-      this.transitions = transitions;
-    }
+
+  constructor({ start, accepts, transitions }) {
+    this.startState = start;
+    this.acceptStates = accepts;
+    this.transitions = transitions;
   }
+
+  accept(FEED) {
+    let state = this.startState;
+    const tape = new Tape(FEED);
+
+debugger
+    while (!this.accepted(state)) {
+      const transition = this.transition(state, tape.read());
+      if (!transition) {
+        return false;
+      }
+      state = transition["newState"];
+      tape.write(transition["write"]);
+      tape.move(transition["direction"]);
+    }
+    return true;
+  }
+
+
+  transition(state, sym) {
+    return (this.transitions[state]||{})[sym];
+  }
+
+  accepted(state) {
+    return this.acceptStates.includes(state)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
